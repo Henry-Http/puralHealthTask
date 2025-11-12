57a2102a82c72c2ce2b8c8790a146812817afd38
Henry-Http <henrybrownonline@yahoo.com>
2025-11-13 00:46:49 +0100

    finished project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,9 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-Cypress.Commands.add('login', (username, password) => {
-  cy.get('#user-name').clear().type(username);
-  cy.get('#password').clear().type(password);
-  cy.get('#login-button').click();
-});
